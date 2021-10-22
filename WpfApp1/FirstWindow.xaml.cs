ffb29546bb62afa01460173f061e181d4f176987
Egemen <egemensener98@gmail.com>
2021-10-22 18:33:31 +0300

    first commit

---
+++ WpfApp1/FirstWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for FirstWindow.xaml
+    /// </summary>
+    public partial class FirstWindow : Window
+    {
+        public FirstWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Back_Menu1_Click(object sender, RoutedEventArgs e) 
+        {
+            MainWindow objMainWindow = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            objMainWindow.Show();                           //It provides open main window
+
+        }
+
+        private void Visit_Healthy_Click(object sender, RoutedEventArgs e)
+        {
+            SecondWindow objSecondWindow = new SecondWindow();
+            this.Visibility = Visibility.Hidden;
+            objSecondWindow.Show();
+        }
+
+      
+        int count=1;
+        private void List_Box_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void Jogging_Checked(object sender, RoutedEventArgs e)  // Count for selected items
+        {
+            ++count;
+        }
+
+        private void Running_Checked(object sender, RoutedEventArgs e)
+        {
+            ++count;
+        }
+
+        private void Swimming_Checked(object sender, RoutedEventArgs e)
+        {
+            ++count;
+        }
+
+        private void Play_Tennis_Checked(object sender, RoutedEventArgs e)
+        {
+            ++count;
+        }
+
+        private void Play_Basketball_Checked(object sender, RoutedEventArgs e)
+        {
+            ++count;
+        }
+
+        private void Jogging_Unchecked(object sender, RoutedEventArgs e) // Delete from count when unselect item
+        {
+            if(count > 1)
+            {
+                count--;
+            }
+            
+        }
+
+        private void Running_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (count > 1)
+            {
+                count--;
+            }
+
+        }
+
+        private void Swimming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (count > 1)
+            {
+                count--;
+            }
+
+        }
+
+        private void Play_Tennis_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (count > 1)
+            {
+                count--;
+            }
+
+        }
+
+        private void Play_Basketball_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (count > 1)
+            {
+                count--;
+            }
+
+        }
+        private void Result_Click(object sender, RoutedEventArgs e)  // Showing message for Result Button
+        {
+            if (count > 1)
+            {
+                ThirdWindow objThirdWindow = new ThirdWindow();
+                objThirdWindow.Show();
+               
+
+            }
+            else
+            {
+                Fourth objFourthWindow = new Fourth();
+                objFourthWindow.Show();
+
+            }
+
+        }
+    }
+}
---
+++ WpfApp1/UserControl1.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        double height;
+        double weight;
+
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        // Please use "," during calculation.Otherwise, it will be a wrong calculation.
+        private void Calculate_Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            double Result = (((weight)) / (height * height) );
+
+            BMI_Text_Box.Text = Result.ToString();
+
+            if ( Result >= 18.5)
+            {
+                if(Result <= 25)
+                {
+                    BMI_Text_Box.Background = Brushes.Green;
+
+
+                }
+                else
+                {
+                    BMI_Text_Box.Background = Brushes.Red;
+                }
+            }
+            else
+            {
+                BMI_Text_Box.Background = Brushes.Red;
+                
+            }
+            Result = 0;
+                
+
+        }
+
+        private void BMI_Text_Box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Height_Text_Box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            height = Convert.ToDouble(Height_Text_Box.Text);
+
+        }
+
+        private void Weight_Text_Box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            weight = Convert.ToDouble(Weight_Text_Box.Text);
+        }
+             
+
+        private void BMI_Text_Box_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
